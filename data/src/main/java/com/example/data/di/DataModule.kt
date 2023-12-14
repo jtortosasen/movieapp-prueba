87453723bb4afbfdb8448313aa54679eecd3df8b
Jorge Tortosa Sendra <jorge.tortosa@outlook.com>
2023-12-14 21:38:13 +0100

    add interface to data di module definition

--- data/src/main/java/com/example/data/di/DataModule.kt
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -1,10 +1,11 @@
 package com.example.data.di
 
 import com.example.data.remote.ApiService
+import com.example.domain.data.IApiService
 import com.example.domain.usecases.MoviesUseCase
 import org.koin.dsl.module
 
 val dataModule = module {
-    single { ApiService() }
+    single<IApiService> { ApiService() }
     single { MoviesUseCase(get()) }
 }
